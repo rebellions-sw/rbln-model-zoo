cd3053256a4e90a72ae49ecd03da17caca98dd84
rebel-jongho <jongho.choi@rebellions.ai>
2024-08-30 13:12:40 +0900

    Add distillbert

---
+++ huggingface/question-answering/distilbert/compile.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+from optimum.rbln import RBLNDistilBertForQuestionAnswering
+
+
+def parsing_argument():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="distilbert-base-uncased-distilled-squad",
+        help="(str) type of distilbert",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parsing_argument()
+    model_id = f"distilbert/{args.model_name}"
+
+    # Compile and export
+    model = RBLNDistilBertForQuestionAnswering.from_pretrained(
+        model_id=model_id,
+        export=True,  # export a PyTorch model to RBLN model with optimum
+        rbln_batch_size=1,
+    )
+    # Save compiled results to disk
+    model.save_pretrained(os.path.basename(model_id))
+
+
+if __name__ == "__main__":
+    main()
