96d24d9e726c1b27607373cd6def4aa7ba2f6eee
rebel-jswoo <87816037+rebel-jswoo@users.noreply.github.com>
2024-09-27 16:22:12 +0900

    Add Model: Kobart (#25)

---
+++ huggingface/text2text-generation/kobart/compile.py
@@ -0,0 +1,22 @@
+import os
+from optimum.rbln import RBLNBartForConditionalGeneration
+
+
+def main():
+    model_id = "gogamza/kobart-summarization"
+
+    # Compile and export
+    model = RBLNBartForConditionalGeneration.from_pretrained(
+        model_id=model_id,
+        export=True,  # export a PyTorch model to RBLN model with optimum
+        rbln_batch_size=1,
+        rbln_enc_max_seq_len=1024,
+        rbln_dec_max_seq_len=1024,
+    )
+
+    # Save compiled results to disk
+    model.save_pretrained(os.path.basename(model_id))
+
+
+if __name__ == "__main__":
+    main()
