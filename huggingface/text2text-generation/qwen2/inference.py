b632aed496a2170a204cbded1277a6106a8a6a17
rebel-jongho <jongho.choi@rebellions.ai>
2024-10-28 14:16:53 +0900

    add model: Qwen2

---
+++ huggingface/text2text-generation/qwen2/inference.py
@@ -0,0 +1,45 @@
+import os
+from optimum.rbln import RBLNQwen2ForCausalLM
+from transformers import AutoTokenizer
+
+
+def main():
+    model_id = "Qwen/Qwen2-7B-Instruct"
+
+    # Load compiled model
+    model = RBLNQwen2ForCausalLM.from_pretrained(
+        model_id=os.path.basename(model_id),
+        export=False,
+    )
+
+    # Prepare inputs
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    text = "Give me a short introduction to large language model."
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": text},
+    ]
+    text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    model_inputs = tokenizer([text], return_tensors="pt")
+
+    # Generate tokens
+    output_sequence = model.generate(
+        model_inputs.input_ids, attention_mask=model_inputs.attention_mask, max_length=32768
+    )
+
+    input_len = model_inputs.input_ids.shape[-1]
+    generated_texts = tokenizer.decode(
+        output_sequence[0][input_len:], skip_special_tokens=True, clean_up_tokenization_spaces=True
+    )
+
+    # Show text and result
+    print("--- Text ---")
+    print(text)
+    print("--- Result ---")
+    print(generated_texts)
+
+
+if __name__ == "__main__":
+    main()
