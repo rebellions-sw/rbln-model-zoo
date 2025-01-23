1d5335c42218d2ae3852376c2d7be070b9b28e94
rebel-jongho <jongho.choi@rebellions.ai>
2025-01-23 16:52:09 +0900

    update Llama Family

---
+++ huggingface/text2text-generation/koni/koni-llama3.1-8b/compile.py
@@ -0,0 +1,25 @@
+import os
+
+from optimum.rbln import RBLNLlamaForCausalLM
+
+
+def main():
+    model_id = "KISTI-KONI/KONI-Llama3.1-8B-Instruct-20241024"
+
+    # Compile and export
+    model = RBLNLlamaForCausalLM.from_pretrained(
+        model_id=model_id,
+        export=True,  # export a PyTorch model to RBLN model with optimum
+        rbln_batch_size=1,
+        rbln_max_seq_len=131_072,  # default "max_position_embeddings"
+        rbln_tensor_parallel_size=8,
+        rbln_attn_impl="flash_attn",
+        rbln_kvcache_partition_len=16_384,  # Length of KV cache partitions for flash attention
+    )
+
+    # Save compiled results to disk
+    model.save_pretrained(os.path.basename(model_id))
+
+
+if __name__ == "__main__":
+    main()
---
+++ huggingface/text2text-generation/koni/koni-llama3.1-8b/inference.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+
+from optimum.rbln import RBLNLlamaForCausalLM
+from transformers import AutoTokenizer
+
+
+def parsing_argument():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--text",
+        type=str,
+        default="안녕? 너는 누구야?",
+        help="(str) type, text for generation",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parsing_argument()
+    model_id = "KISTI-KONI/KONI-Llama3.1-8B-Instruct-20241024"
+
+    # Load compiled model
+    model = RBLNLlamaForCausalLM.from_pretrained(
+        model_id=os.path.basename(model_id),
+        export=False,
+    )
+
+    # Prepare inputs
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+    conversation = [{"role": "user", "content": args.text}]
+    text = tokenizer.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+    inputs = tokenizer(text, return_tensors="pt", padding=True)
+
+    terminators = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+    # Generate tokens
+    output_sequence = model.generate(
+        inputs.input_ids,
+        attention_mask=inputs.attention_mask,
+        max_length=131_072,
+        eos_token_id=terminators,
+    )
+
+    input_len = inputs.input_ids.shape[-1]
+    generated_texts = tokenizer.decode(
+        output_sequence[0][input_len:], skip_special_tokens=True, clean_up_tokenization_spaces=True
+    )
+
+    # Show text and result
+    print("--- text ---")
+    print(args.text)
+    print("--- Result ---")
+    print(generated_texts)
+
+
+if __name__ == "__main__":
+    main()
